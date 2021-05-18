405cff968dbd02ada1a17da432e6d1340962f70d
mak6969 <74461188+mak6969@users.noreply.github.com>
2021-05-18 10:47:30 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -55,7 +55,6 @@ if(prefix === null) prefix = default_prefix;
         .addField('`info`', `Key Information. \nArgs: **${prefix}info**`)
 	.addField('`stats`', `Application Statistics. \nArgs: **${prefix}stats**`)
         .addField('`reset`', `Reset key. \nArgs: **${prefix}reset**`)
-	.addField('`resethash`', `Reset App Hash. \nArgs: **${prefix}resethash**`)
         .setFooter('KeyAuth Discord Bot', client.user.displayAvatarURL())
         .setTimestamp()
 
